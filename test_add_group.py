ae1c3e5d2680692e1d9d3bebcba0cb87501727a1
Yegorova43 <egorovamaria91@mail.ru>
2017-05-30 19:38:04 +0300

    Запущенный тест

--- test_add_group.py
+++ test_add_group.py
@@ -18,7 +18,10 @@ class test_add_group(unittest.TestCase):
     def test_test_add_group(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -30,15 +33,14 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("прерке")
+        wd.find_element_by_name("group_name").send_keys("efrefre")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("ерере")
+        wd.find_element_by_name("group_header").send_keys("ref")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("перн")
+        wd.find_element_by_name("group_footer").send_keys("ergrg")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
     
