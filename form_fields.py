0e085901a9971fee36c35a000c09e890ab5e632a
Yegorova43 <egorovamaria91@mail.ru>
2017-05-31 21:39:59 +0300

    Создание класса Form_fields

---
+++ form_fields.py
@@ -0,0 +1,24 @@
+class Form_fields:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work,
+                       fax, email, email2, email3, homepage, address2, phone2, notes, byear, ayear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+        self.byear = byear
+        self.ayear = ayear
