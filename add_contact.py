22158b35539c95374d5e8ff35c4c84d11d5fa7a3
Yegorova43 <egorovamaria91@mail.ru>
2017-06-05 22:54:57 +0300

    Добавлена фикстура

--- add_contact.py
+++ add_contact.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from form_fields import Form_fields
+from application_contact import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture=Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_add_contact(self):
-        success = True
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Form_fields(firstname="Мария", middlename="Александровна", lastname="Егорова",
+
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Form_fields(firstname="Мария", middlename="Александровна", lastname="Егорова",
                                             nickname="Nick",
                                             title="Hello", company="Company", address="Address", home="54",
                                             mobile="89317856906",
@@ -33,127 +25,4 @@ class add_contact(unittest.TestCase):
                                             Bmonth="//div[@id='content']/form/select[2]//option[4]",
                                             Aday="//div[@id='content']/form/select[3]//option[16]",
                                             Amonth="//div[@id='content']/form/select[4]//option[12]"))
-        self.logout(wd)
-        self.assertTrue(success)
-
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_contact(self, wd, form_fields):
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill first name
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(form_fields.firstname)
-        # fill middle name
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(form_fields.middlename)
-        # fill last name
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(form_fields.lastname)
-        # fill nickname
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(form_fields.nickname)
-        # fill title
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(form_fields.title)
-        # fill company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(form_fields.company)
-        # fill address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(form_fields.address)
-        # fill home
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(form_fields.home)
-        # fill mobile
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(form_fields.mobile)
-        # fill work
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(form_fields.work)
-        # fill fax
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(form_fields.fax)
-        # fill email
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(form_fields.email)
-        # fill email2
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(form_fields.email2)
-        # fill email3
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(form_fields.email3)
-        # fill homepage
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(form_fields.homepage)
-        # fill birthday
-        if not wd.find_element_by_xpath(form_fields.Bday).is_selected():
-            wd.find_element_by_xpath(form_fields.Bday).click()
-        if not wd.find_element_by_xpath(form_fields.Bmonth).is_selected():
-            wd.find_element_by_xpath(form_fields.Bmonth).click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(form_fields.byear)
-        # fill anniversary
-        if not wd.find_element_by_xpath(form_fields.Aday).is_selected():
-            wd.find_element_by_xpath(form_fields.Aday).click()
-        if not wd.find_element_by_xpath(form_fields.Amonth).is_selected():
-            wd.find_element_by_xpath(form_fields.Amonth).click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(form_fields.ayear)
-        # fill address2
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(form_fields.address2)
-        # fill phone2
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(form_fields.phone2)
-        # fill notes
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(form_fields.notes)
-        # submit contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def company(self):
-        return "еглгешл"
-
-    def login(self, wd, username, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+    app.logout()
