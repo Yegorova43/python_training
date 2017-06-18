e30872c13ef7f9f77060d032bab2e92926759d71
Yegorova43 <egorovamaria91@mail.ru>
2017-06-18 15:52:21 +0300

    Оптимизация операции с переходами

--- fixture/group.py
+++ fixture/group.py
@@ -5,14 +5,14 @@ class GroupHelper:
 
     def return_to_groups_page(self):
         wd = self.app.wd
-        #open groups page
+        # open groups page
         wd.find_element_by_link_text("group page").click()
 
-    def delete_first_group (self):
+    def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
-        #submit deletion
+        # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
@@ -24,26 +24,26 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
-        #open modification form
+        # open modification form
         wd.find_element_by_name("edit").click()
-        #fill group form
+        # fill group form
         self.fill_group_form(new_group_data)
-        #submit modification
+        # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def submit_group_creation(self):
         wd = self.app.wd
-        #submit group creation
+        # submit group creation
         wd.find_element_by_name("submit").click()
 
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        #init group creation
+        # init group creation
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
-        #submit group creation
+        # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
@@ -62,9 +62,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        #open groups page
-        wd.find_element_by_link_text("groups").click()
-
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def count(self):
         wd = self.app.wd
