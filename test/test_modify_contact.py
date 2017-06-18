77763552fd293daa580ec37e18056b1cf6348502
Yegorova43 <egorovamaria91@mail.ru>
2017-06-18 15:05:24 +0300

    Созданы предусловия для тестов на модификацию групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,11 +3,11 @@ from model.contact import Contact
 
 def test_modify_first_contact_firstname(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="test"))
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New_firstname"))
 
 
 def test_modify_first_contact_middlename(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="test"))
+        app.contact.create(Contact(middlename="test"))
     app.contact.modify_first_contact(Contact(middlename="New_middlename"))
